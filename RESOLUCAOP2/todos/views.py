451d13d218353c604e16380fd866065add3e77fa
Yuri Stefan <yuri_guedes10@hotmail.com>
2025-06-12 19:41:13 -0300

    Implementação da Listagem de tarefas (ListView + Template + URL)

--- RESOLUCAOP2/todos/views.py
+++ RESOLUCAOP2/todos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Todo
 
-# Create your views here.
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title','deadline']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo_list')
