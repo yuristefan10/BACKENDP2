79b2667250edb5277dfbda99055ee8203ef2a396
Yuri Stefan <yuri_guedes10@hotmail.com>
2025-06-12 19:59:31 -0300

    Implementação da Criação de Tarefas(Createview + Update + Delet

--- RESOLUCAOP2/setup/urls.py
+++ RESOLUCAOP2/setup/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', TodoListView.as_view(), name='todo_list'),
     path('create/', TodoCreateView.as_view(), name='todo_create'),
     path('update/<int:pk>/', TodoUpdateView.as_view(), name='todo_update'),
-    path('delete/<int:pk>/', TodoDeleteView.as_view(), name='todo_delete')
+    path('delete/<int:pk>/', TodoDeleteView.as_view(), name='todo_delete'),
 
 ]
 
